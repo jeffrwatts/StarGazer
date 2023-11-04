cf66978df3f1d1e4a53106136c14b56ae93c8082
jeffrwatts <jeffrwatts@gmail.com>
2023-11-03 19:36:19 -0700

    Updated SightsViewModel to use LocationRepsoitory, still some question about listeners yet to resolve.

--- app/src/main/java/com/jeffrwatts/stargazer/ui/sights/SightsViewModel.kt
+++ app/src/main/java/com/jeffrwatts/stargazer/ui/sights/SightsViewModel.kt
@@ -1,49 +1,52 @@
 package com.jeffrwatts.stargazer.ui.sights
-
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.jeffrwatts.stargazer.data.celestialobject.CelestialObj
-import com.jeffrwatts.stargazer.data.celestialobject.CelestialObjPos
-import com.jeffrwatts.stargazer.data.celestialobject.CelestialObjRepository
-import com.jeffrwatts.stargazer.data.celestialobject.ObjectType
-import com.jeffrwatts.stargazer.data.celestialobject.ObservationStatus
+import com.jeffrwatts.stargazer.data.celestialobject.*
 import com.jeffrwatts.stargazer.data.location.LocationRepository
 import com.jeffrwatts.stargazer.utils.Utils
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.flow.*
 import kotlinx.coroutines.launch
 
 class SightsViewModel(
     private val repository: CelestialObjRepository,
     private val locationRepository: LocationRepository
 ) : ViewModel() {
-    companion object {
-        const val LATITUDE = 19.639994  // Example: Kona's latitude
-        const val LONGITUDE = -155.996926 // Example: Kona's longitude
-    }
 
     private val _uiState = MutableStateFlow<SightsUiState>(SightsUiState.Loading)
     val uiState: StateFlow<SightsUiState> = _uiState
 
     init {
+        //locationRepository.startLocationUpdates(viewModelScope)
         fetchObjects()
     }
 
     fun fetchObjects() {
         viewModelScope.launch {
-            _uiState.value = SightsUiState.Loading
-
-            runCatching {
-                val jdNow = Utils.calculateJulianDateNow();
-
-                val objects = repository.getAllStream().first()
-                    .filterNot { it.type == ObjectType.STAR }
-                objects.map { obj ->
-                    CelestialObjPos.fromCelestialObj(obj, julianDate = jdNow, lat = LATITUDE, lon = LONGITUDE)
-                }.sortedByDescending { it.alt }
-            }.onSuccess { _uiState.value = SightsUiState.Success(it) }
-                .onFailure { _uiState.value = SightsUiState.Error(it.message ?: "Unknown error") }
+            repository.getAllStream()
+                .combine(locationRepository.locationFlow) { objects, location ->
+                    // Check if location is not null and proceed
+                    if (location != null) {
+                        val jdNow = Utils.calculateJulianDateNow()
+                        objects.filterNot { it.type == ObjectType.STAR }
+                            .map { obj ->
+                                CelestialObjPos.fromCelestialObj(obj, julianDate = jdNow, lat = location.latitude, lon = location.longitude)
+                            }.sortedByDescending { it.alt }
+                    } else {
+                        // Emit a Loading state if the location is not yet available
+                        _uiState.value = SightsUiState.Loading
+                        return@combine emptyList<CelestialObjPos>()
+                    }
+                }
+                .distinctUntilChanged() // To avoid redundant UI updates
+                .catch { e ->
+                    _uiState.value = SightsUiState.Error(e.message ?: "Unknown error")
+                }
+                .collect { celestialObjPosList ->
+                    // If location is still not available (null), keep the Loading state
+                    if (celestialObjPosList.isNotEmpty() || locationRepository.locationFlow.value != null) {
+                        _uiState.value = SightsUiState.Success(celestialObjPosList)
+                    }
+                }
         }
     }
 
@@ -53,6 +56,11 @@ class SightsViewModel(
             repository.update(updatedItem)
         }
     }
+
+    override fun onCleared() {
+        super.onCleared()
+        //locationRepository.stopLocationUpdates()
+    }
 }
 
 sealed class SightsUiState {
@@ -60,4 +68,3 @@ sealed class SightsUiState {
     data class Success(val data: List<CelestialObjPos>) : SightsUiState()
     data class Error(val message: String) : SightsUiState()
 }
-
