0f8fbb0ae4a29068b7acce96847690e97d1f15e7
jeffrwatts <jeffrwatts@gmail.com>
2024-05-28 18:10:23 -0700

    Updated Dependencies with newer versions

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -78,10 +78,10 @@ android {
 }
 
 dependencies {
-    implementation("androidx.core:core-ktx:1.12.0")
+    implementation("androidx.core:core-ktx:1.13.1")
 
-    implementation("androidx.activity:activity-compose:1.8.2")
-    implementation(platform("androidx.compose:compose-bom:2023.06.01"))
+    implementation("androidx.activity:activity-compose:1.9.0")
+    implementation(platform("androidx.compose:compose-bom:2024.05.00"))
 
     implementation("androidx.compose.material3:material3")
     implementation("androidx.compose.material:material")
@@ -92,8 +92,8 @@ dependencies {
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
 
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
-    implementation("androidx.lifecycle:lifecycle-runtime-compose:2.7.0")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.0")
+    implementation("androidx.lifecycle:lifecycle-runtime-compose:2.8.0")
     implementation("androidx.navigation:navigation-compose:2.7.7")
     implementation ("androidx.swiperefreshlayout:swiperefreshlayout:1.1.0")
 
@@ -101,10 +101,10 @@ dependencies {
 
 
     // CameraX
-    implementation ("androidx.camera:camera-camera2:1.3.2")
-    implementation ("androidx.camera:camera-lifecycle:1.3.2")
-    implementation ("androidx.camera:camera-view:1.4.0-alpha04")
-    implementation ("androidx.camera:camera-extensions:$1.4.0-alpha04")
+    implementation ("androidx.camera:camera-camera2:1.3.3")
+    implementation ("androidx.camera:camera-lifecycle:1.3.3")
+    implementation ("androidx.camera:camera-view:1.4.0-beta01")
+    implementation ("androidx.camera:camera-extensions:1.4.0-beta01")
 
     implementation("io.coil-kt:coil:2.6.0")
     implementation("io.coil-kt:coil-compose:2.6.0")
@@ -123,7 +123,7 @@ dependencies {
     implementation ("com.google.accompanist:accompanist-permissions:0.32.0")
 
     // Serialization
-    implementation("com.google.code.gson:gson:2.9.0")
+    implementation("com.google.code.gson:gson:2.10.1")
 
     // retrofit for networking
     implementation ("com.squareup.retrofit2:retrofit:2.9.0")
@@ -145,7 +145,7 @@ dependencies {
     androidTestImplementation ("androidx.test:runner:1.5.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    androidTestImplementation(platform("androidx.compose:compose-bom:2023.03.00"))
+    androidTestImplementation(platform("androidx.compose:compose-bom:2024.05.00"))
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     testImplementation ("androidx.arch.core:core-testing:2.2.0")
     testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.7.3")
